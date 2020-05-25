b60ee0fe738d2f32900affaf823c1b512f8f310b
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-05-25 10:42:04 +0200

    :art: Bundler > Common > Improve structure

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -24,6 +24,13 @@ module.exports = {
     {
         rules:
         [
+            // HTML
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+
+            // CSS
             {
                 test: /\.css$/,
                 use:
@@ -32,6 +39,8 @@ module.exports = {
                     'css-loader'
                 ]
             },
+
+            // Stylus
             {
                 test: /\.styl$/,
                 use:
@@ -41,6 +50,18 @@ module.exports = {
                     'stylus-loader'
                 ]
             },
+
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+
+            // Images
             {
                 test: /\.(jpg|png|gif|svg)$/,
                 use:
@@ -49,11 +70,13 @@ module.exports = {
                         loader: 'file-loader',
                         options:
                         {
-                            outputPath: 'images/'
+                            outputPath: 'assets/images/'
                         }
                     }
                 ]
             },
+
+            // Fonts
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use:
@@ -62,11 +85,13 @@ module.exports = {
                         loader: 'file-loader',
                         options:
                         {
-                            outputPath: 'fonts/'
+                            outputPath: 'assets/fonts/'
                         }
                     }
                 ]
             },
+
+            // Audios
             {
                 test: /\.(mp3|wav|ogg)$/,
                 use:
@@ -75,21 +100,9 @@ module.exports = {
                         loader: 'file-loader',
                         options:
                         {
-                            outputPath: 'audios/'
+                            outputPath: 'assets/audios/'
                         }
                     }
-                ]
-            },
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
                 ]
             }
         ]
